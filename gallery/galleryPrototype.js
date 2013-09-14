8970d5a4d42f0a0e79ccfe31840711bb737835ee
aleksuk <aleksuk.m@gmail.com>
2013-09-14 16:18:20 +0300

    edit gallery

--- gallery/galleryPrototype.js
+++ gallery/galleryPrototype.js
@@ -79,129 +79,150 @@ function removeClass(node, className) {
 	}
 	node.className = newClass.join(' ');
 }
+var Gallery = (function () {
+	var activeGallery;
 
-function Gallery(node, largePic) {
-	var navigation = node.querySelectorAll('.navigation a');
-	this.previews = node.querySelector('.previews');
-	this.large = node.querySelector('.big-picture img');
-	this.largeImgPath = 'img/large/';
-	this.rotateTimer = null;
-	this.largePicNames = largePic;
-	var self = this;
-	this.galleryNode = node;
-	
-	var next = function (event) {
-		preventDef(event);
-		self.nextPicture();
-		},
-		prev = function (event) {
-			preventDef(event);
-			self.prevPicture();
-		},
-		select = function (event) {
-			event = event || window.event;
-			var target = event.target || event.srcElemen;
-			
-			preventDef(event);	
-			if (target.nodeName !== 'UL') {
-				target = closest(target, function  (node) {
-					return node.nodeName === 'LI';
-				});
+	function Gallery(node, largePic) {
+		var navigation = node.querySelectorAll('.navigation a'),
+			self = this;
+
+		this.previews = node.querySelector('.previews');
+		this.large = node.querySelector('.big-picture img');
+		this.largeImgPath = 'img/large/';
+		this.rotateTimer = null;
+		this.largePicNames = largePic;
+		this.galleryNode = node;
+		
+		if (!activeGallery) {
+			activeGallery = this;
+		}
+
+		var handler = (function () {
+				return {
+					next : function (event) {
+						preventDef(event);
+						self.nextPicture();
+					},
+					prev : function (event) {
+						preventDef(event);
+						self.prevPicture();
+					},
+					select: function (event) {
+						event = event || window.event;
+						var target = event.target || event.srcElemen;
+						
+						preventDef(event);	
+						if (target.nodeName !== 'UL') {
+							target = closest(target, function  (node) {
+								return node.nodeName === 'LI';
+							});
+						}
+						self.selectPicture(target); 
+					},
+					start : function () {
+						self.startAutorotating();
+					},
+					press : function (event) {
+						event = event || window.event;
+
+						self.scrolling(event);
+					},
+					setActiveGallery : function(event) {
+						activeGallery = self;
+					}
+				}
+			})();
+
+		addEvent(navigation[1], 'click', handler.next);
+		addEvent(navigation[0], 'click', handler.prev);
+		addEvent(this.previews, 'click', handler.select);
+		addEvent(document.documentElement, 'keydown', handler.press);
+		addEvent(this.galleryNode, 'mouseover', handler.setActiveGallery);
+
+	this.startAutorotating();	
+	}
+
+	Gallery.prototype.nextPicture = function () {
+		var nextCurrentImg = 0;	
+
+		for (var i = 0; i < this.previews.children.length; i += 1) {
+			if ( hasClass(this.previews.children[i], 'current') ) {
+				removeClass(this.previews.children[i], 'current');
+				nextCurrentImg = i + 1;
 			}
-			self.selectPicture(target); 
-		},
-		start = function () {
-			self.startAutorotating();
-		},
-		press = function (event) {
-			event = event || window.event;
-			var target = event.target || event.srcElemen;
-
-			self.scrolling(event);
 		}
-
-	addEvent(navigation[1], 'click', next);
-	addEvent(navigation[0], 'click', prev);
-	addEvent(this.previews, 'click', select);
-	addClass(this.galleryNode, 'keydown', press);
-this.startAutorotating();
-	
-}
-
-Gallery.prototype.nextPicture = function () {
-	var nextCurrentImg = 0;	
-
-	for (var i = 0; i < this.previews.children.length; i += 1) {
-		if ( hasClass(this.previews.children[i], 'current') ) {
-			removeClass(this.previews.children[i], 'current');
-			nextCurrentImg = i + 1;
+		
+		if (nextCurrentImg < this.previews.children.length) {
+			addClass(this.previews.children[nextCurrentImg], 'current');
+			this.large.src = this.largeImgPath + this.largePicNames[nextCurrentImg];
+		} else {
+			addClass(this.previews.children[0], 'current');
+			this.large.src = this.largeImgPath + this.largePicNames[0];
 		}
+		this.stopAutorotating();
+	}
+
+	Gallery.prototype.prevPicture = function () {
+		var prevCurrentImg = 0;	
+		
+		for (var i = 0; i < this.previews.children.length; i += 1) {
+			if ( hasClass(this.previews.children[i], 'current') ) {
+				removeClass(this.previews.children[i], 'current');
+				prevCurrentImg = i - 1;
+			}
+		}
+		
+		if (prevCurrentImg < 0) {
+			addClass(this.previews.children[this.previews.children.length - 1], 'current');
+			this.large.src = this.largeImgPath + this.largePicNames[this.previews.children.length - 1];
+		} else {
+			addClass(this.previews.children[prevCurrentImg], 'current');
+			this.large.src = this.largeImgPath + this.largePicNames[prevCurrentImg];
+		}
+		this.stopAutorotating();
+	}
+
+	Gallery.prototype.selectPicture = function (target) {
+
+		for (var i = 0; i < this.previews.children.length; i += 1) {
+			if ( hasClass(this.previews.children[i], 'current') ) {
+				removeClass(this.previews.children[i], 'current');
+			}
+			if (target === this.previews.children[i]) {
+				addClass(this.previews.children[i], 'current');
+				this.large.src = this.largeImgPath + this.largePicNames[i];
+			}
+		}
+		this.stopAutorotating();
+	}
+
+	Gallery.prototype.startAutorotating = function () {
+		var self = this;
+		this.rotateTimer = setInterval(function () { 
+			self.nextPicture() 
+		}, 5000);
+	}
+
+	Gallery.prototype.stopAutorotating = function () {
+		clearInterval(this.rotateTimer);
+		this.startAutorotating();
 	}
 	
-	if (nextCurrentImg < this.previews.children.length) {
-		addClass(this.previews.children[nextCurrentImg], 'current');
-		this.large.src = this.largeImgPath + this.largePicNames[nextCurrentImg];
-	} else {
-		addClass(this.previews.children[0], 'current');
-		this.large.src = this.largeImgPath + this.largePicNames[0];
-	}
-	this.stopAutorotating();
-}
-
-Gallery.prototype.prevPicture = function () {
-	var prevCurrentImg = 0;	
-	
-	for (var i = 0; i < this.previews.children.length; i += 1) {
-		if ( hasClass(this.previews.children[i], 'current') ) {
-			removeClass(this.previews.children[i], 'current');
-			prevCurrentImg = i - 1;
+	Gallery.prototype.scrolling = function(target) {
+		var LEFT_ARROW = 37,
+			RIGHT_ARROW = 39;
+		if (activeGallery === this) {
+			if (target.keyCode === LEFT_ARROW) {
+				this.prevPicture();
+			} else if (target.keyCode === RIGHT_ARROW) {
+				this.nextPicture();
+			}
 		}
 	}
-	
-	if (prevCurrentImg < 0) {
-		addClass(this.previews.children[this.previews.children.length - 1], 'current');
-		this.large.src = this.largeImgPath + this.largePicNames[this.previews.children.length - 1];
-	} else {
-		addClass(this.previews.children[prevCurrentImg], 'current');
-		this.large.src = this.largeImgPath + this.largePicNames[prevCurrentImg];
-	}
-	this.stopAutorotating();
-}
+		
+	return Gallery;
+})();
 
-Gallery.prototype.selectPicture = function (target) {
-
-	for (var i = 0; i < this.previews.children.length; i += 1) {
-		if ( hasClass(this.previews.children[i], 'current') ) {
-			removeClass(this.previews.children[i], 'current');
-		}
-		if (target === this.previews.children[i]) {
-			addClass(this.previews.children[i], 'current');
-			this.large.src = this.largeImgPath + this.largePicNames[i];
-		}
-	}
-	this.stopAutorotating();
-}
-
-Gallery.prototype.scrolling = function(target) {
-	var LEFT_ARROW = 37,
-		RIGHT_ARROW = 39;
-
-	if (target.keyCode === LEFT_ARROW) {
-		this.prevPicture();
-	} else if (target.keyCode === RIGHT_ARROW) {
-		this.nextPicture();
-	}	
-}
-Gallery.prototype.startAutorotating = function () {
-	var self = this;
-	this.rotateTimer = setInterval(function () { 
-		self.nextPicture() 
-	}, 5000);
-}
-Gallery.prototype.stopAutorotating = function () {
-	clearInterval(this.rotateTimer);
-	this.startAutorotating();
-}
 function windowOnLoad() {
 	var bigPictures = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg', 'pic6.jpg'],
 		gallery = new Gallery(document.getElementById('first'), bigPictures),
